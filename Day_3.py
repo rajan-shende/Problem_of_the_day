b382bfb76056a7b0b3ba28dba2b34417e9901c0a
RAJAN <75211405+rajan-shende@users.noreply.github.com>
2026-01-24 20:23:54 +0530

    Delete Day_3.py

--- Day_3.py
+++
@@ -1,9 +0,0 @@
-# Reversse the string without reverse function or any built in function
-str2 = "hello"
-def rev_str(input_str):
-	a = ""
-	for i in input_str:
-		a = i+a 
-	return a
-
-print(rev_str(str2))
