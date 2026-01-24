11f334672cd088966424d90105638a5a2fd70511
RAJAN <75211405+rajan-shende@users.noreply.github.com>
2026-01-24 20:25:23 +0530

    Delete Day_5.py

--- Day_5.py
+++
@@ -1,9 +0,0 @@
-# Reverse a string from specified index onwards
-def reverse_at(str_in, id):
-	splited = [a for a in str_in ]
-	rev = splited[id:][::-1]
-	fwd = splited[:id]
-	final = fwd+rev
-	return final
-
-print(reverse_at("KILL-BILL",4))
