3576234e7897b4ebe5c1f3355ee957f4cbef6cc1
RAJAN <75211405+rajan-shende@users.noreply.github.com>
2026-01-25 16:49:36 +0530

    Update Day_4.py

--- CPPy/Day_4.py
+++ CPPy/Day_4.py
@@ -1,3 +1,4 @@
+# Solution 1
 # Move all 0 to end of the list
 list_z = [10,0,20,30,0,40,0,50]
 
@@ -13,3 +14,16 @@ def move_zeros(input_list):
 	return non_zeros
 	
 print(move_zeros(list_z))
+
+# Solution 2
+# Take all zeros to the end of list
+list2 = [10,0,20,0,30,0]
+
+def move_zero_to_end(input_list):
+    for a in input_list:
+        if a == 0:
+            input_list.remove(0)
+            input_list.append(0)
+    print(input_list)
+
+move_zero_to_end(list2)
