c3dffcfa0d2fe8c088d6fc54ffc37cb2ab159073
RAJAN <75211405+rajan-shende@users.noreply.github.com>
2026-01-27 18:33:26 +0530

    Create Day_11.py

---
+++ CPPy/Day_11.py
@@ -0,0 +1,18 @@
+# Find out duplicates from given list
+l1 = [10,20,30,20,40,30,50]
+
+def get_dupes(in_list):
+    dupes = []
+    Dp = {}
+    for a in in_list:
+        if a not in Dp:
+            Dp[a] = 1
+        else:
+            Dp[a] += 1
+    for a,b in Dp.items():
+        if b > 1:
+            dupes.append(a)
+    print(dupes)
+
+
+get_dupes(l1)
